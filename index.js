825d71c7476efa1c86addbcd0e2bfbf1a0115c85
Matthew Crooks <mfcrooks@gmail.com>
2020-08-06 18:03:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,11 +14,10 @@ document.addEventListener("keydown", function(e) {
 });
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
-
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
   }
 }
 document.addEventListener("keydown", function(e) {
